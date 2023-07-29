a0e784d7e5d8b080df11486c3b9e265808a469e3
Abin John <abinjohnanto@gmail.com>
2023-07-29 19:48:08 +0530

    feat: delete playlists

--- src/Components/Playlists/PlayListCard.jsx
+++ src/Components/Playlists/PlayListCard.jsx
@@ -1,8 +1,17 @@
 import { useNavigate } from 'react-router';
+import { DeleteIcon } from '../../icons/svg';
+import { useAppContext } from '../../AppContext';
 
 const PlayListCard = ({ playlist }) => {
   const { name, description } = playlist;
+  const { playLists, setPlaylists } = useAppContext();
   const navigate = useNavigate();
+
+  const handleDeleteClick = (e) => {
+    e.stopPropagation();
+
+    setPlaylists(playLists.filter((item) => item.id !== playlist.id));
+  };
 
   return (
     <div
@@ -12,9 +21,14 @@ const PlayListCard = ({ playlist }) => {
       <div className="img-container">
         <img src="https://picsum.photos/250/150" alt={name} />
       </div>
-      <div className="details">
-        <h4 className="title">{name}</h4>
-        {description && <p className="desc">{description}</p>}
+      <div className="action">
+        <div className="details">
+          <h4 className="title">{name}</h4>
+          {description && <p className="desc">{description}</p>}
+        </div>
+        <div onClick={handleDeleteClick}>
+          <DeleteIcon />
+        </div>
       </div>
     </div>
   );
